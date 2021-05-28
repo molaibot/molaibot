68ccb78370e5c58cec627a64086a2073e4e32d1e
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-05-28 21:52:30 +0500

    Fix the suggest command

--- commands/feedback/suggest.js
+++ commands/feedback/suggest.js
@@ -7,7 +7,7 @@ module.exports = {
   run: async(client, message, args) => {
       const sc = client.channels.cache.get("847586623763775500")
 
-      const s = args[0].join(" ");
+      const s = args.slice(0).join(" ");
 
       const embed1 = new MessageEmbed()
       .setAuthor(message.author.tag)
