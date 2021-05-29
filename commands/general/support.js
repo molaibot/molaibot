bd973ec1160a1c58df4e2e94fa5bffb67541f4b4
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-05-29 22:43:03 +0500

    Add support command

---
+++ commands/general/support.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+module.exports = {
+  name: 'support',
+  description: 'Get support',
+  aliases: ['supportserver'],
+  run: async(client, message, args) => {
+    fetch(`https://discord.com/api/v9/channels/${message.channel.id}/messages`, {
+        method: 'POST',
+        body: JSON.stringify({"content":"**Wanna Join The Support Server For MolaiBOT And An Awesome Community?**",
+            "components": [{
+                "type": 1,
+                "components": [
+                    {
+                        "type": 2,
+                        "label": 'Join The Server',
+                        "style": 5,
+                        "url": "https://discord.gg/NRthkRKEXw"
+                    }
+                ]
+            }]
+    }),
+    headers: {
+        "Authorization": `Bot ${client.token}`,
+        "Content-Type": "application/json"
+    }
+    }).then(res => res.json());
+  }
+}
