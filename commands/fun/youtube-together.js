9e484c005e01d6083503058c3b75ea69d7bed239
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-05-31 00:56:43 +0500

    Add an embed on the yt command

--- commands/fun/youtube-together.js
+++ commands/fun/youtube-together.js
@@ -1,12 +1,20 @@
 const fetch = require('node-fetch');
 const { MessageEmbed } = require('discord.js');
+const e = require('../../utils/embeds.json');
 module.exports = {
   name: 'youtube-together',
   description: 'Watch youtube together!',
-  aliases: ['yt-together', 'yt-t'],
+  aliases: ['yt-together', 'yt-t', 'youtube'],
   usage: 'Join a voice chat and run the command to watch youtube in a voice chat.',
   cooldown: 10000,
   run: async(client, message, args) => {
+    const embed = new MessageEmbed
+    .setColor(e.color)
+    .setFooter(e.footer)
+    .setTitle('Watch YouTube Together')
+    .setTimestamp()
+    .setDescription('To start watching, click: **Start watching**')
+
     let channel = message.member.voice.channel; 
     if(!channel) return message.channel.send("You need to be in a voice chat to use youtube together.") 
 
@@ -29,9 +37,11 @@ module.exports = {
     .then(res => res.json())
     .then(invite => { 
         if(!invite.code) return message.channel.send(errorEmbed)
+
+        message.channel.send(embed);
 	fetch(`https://discord.com/api/v9/channels/${message.channel.id}/messages`, {
     method: "POST",
-    body: JSON.stringify({"content":"**Watch YouTube Together!**",
+    body: JSON.stringify({"content":"**Start Watching**",
         "components": [{
             "type": 1,
             "components": [
