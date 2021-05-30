9fc33f47d822f4fc90f4e53696684fdaf21f45dd
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-05-30 16:28:18 +0500

    Fix more and more bugsss

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,9 +1,8 @@
-const { MessageEmbed } = require('discord.js');
 const profileModel = require('../../models/profileSchema');
 module.exports = {
   name: 'daily',
   description: 'Get daily rewards!',
-  //cooldown: 43200000,
+  cooldown: 43200000,
   run: async(client, message, args, profileData) => {
     const dailyReward = 1500;
 
@@ -13,25 +12,12 @@ module.exports = {
         userID: message.author.id
     }
 
-    try{
     profileModel.findOneAndUpdate(params, {
         $inc: {
             coins: dailyReward
         }
-    })
-    if(profileData.coins = 0) {
-      message.channel.send(`You claimed your rewards for today, come back in 12 hours! Your wallet balance is: **1500**.`);
-    } else {
-    message.channel.send(`You claimed your rewards for today, come back in 12 hours! Your wallet balance is: **${newBal}**.`);
-    }
-  }catch(err){
-    console.log(err)
-    const errEmbed = new MessageEmbed()
-    .setColor('#37393e')
-    .setTitle('Error!')
-    .setDescription(err);
-
-    message.channel.send(errEmbed);
-  }
+    }).then(
+      message.channel.send(`You claimed your daily rewards for today, You recieved 1500 coins! **Your balance now is: ${newBal}**, come back in 12 hours for more!`)
+    );
   }
 }
