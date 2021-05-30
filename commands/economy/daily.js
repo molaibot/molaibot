7459c3efba5e63259866c6998f653cd03d0b61f6
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-05-30 16:14:48 +0500

    Fix bugs

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -2,11 +2,14 @@ const profileModel = require('../../models/profileSchema');
 module.exports = {
   name: 'daily',
   description: 'Get daily rewards!',
-  cooldown: 43200000,
+  //cooldown: 43200000,
   run: async(client, message, args, profileData) => {
     const dailyReward = 1500;
 
     let newBal = profileData.coins + dailyReward;
+    if(profileData > 0) {
+      newBal = dailyReward;
+    }
 
     const params = {
         userID: message.author.id
@@ -17,7 +20,7 @@ module.exports = {
             coins: dailyReward
         }
     }).then(
-      message.channel.send(`You claimed your daily rewards for today, You recieved 1500 coins! **Your balance now is: ${newBal}**, come back in 12 hours for more!`)
+      message.channel.send(`You claimed your daily rewards for today, You recieved 1500 coins! **Your wallet balance now is: ${newBal}**, come back in 12 hours for more!`)
     );
   }
 }
