46b5810c1e073a0abc12e415f9095e37ee50c0c1
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-06-02 23:41:21 +0500

    We're Switching Domains, Going From molaibot.ml To bot.molai.dev! + website command

--- commands/utilities/website.js
+++ commands/utilities/website.js
@@ -1,19 +1,19 @@
 const fetch = require('node-fetch');
 module.exports = {
-  name: 'commands',
-  description: 'Link to the commands page',
+  name: 'website',
+  description: 'Link to the molaibot website',
   run: async(client, message, args) => {
     fetch(`https://discord.com/api/v9/channels/${message.channel.id}/messages`, {
         method: "POST",
-        body: JSON.stringify({"content":"**Wanna Visit The Commands Page?**",
+        body: JSON.stringify({"content":"**Wanna Visit The Website?**",
             "components": [{
                 "type": 1,
                 "components": [
                     {
                         "type": 2,
-                        "label": "Click Me To Visit!",
+                        "label": "Click Me To Visit",
                         "style": 5,
-                         "url": "https://bot.molai.dev/commands"
+                         "url": "https://bot.molai.dev/"
                     }
                 ]
     
