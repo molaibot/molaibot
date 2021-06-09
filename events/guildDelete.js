aba869237831215ea916e228af012e82501ca256
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-06-09 23:18:18 +0500

    We switched most events to the events dir except for the message event

---
+++ events/guildDelete.js
@@ -0,0 +1,13 @@
+module.exports = (guild) => {
+    const onLeaveChannel = client.channels.cache.get('833903791321120858');
+
+	const onLeaveEmbed = new Discord.MessageEmbed()
+		.setTitle(':cry: MolaiBOT Kicked :cry:')
+		.addField('Guild', '```' + guild.name + '```')
+		.addField('Server Region', '```' + guild.region + '```', true)
+		.addField('Guild ID', '```' + guild.id + '```', true)
+		.addField('Guild Owner ID', '```' + guild.ownerID + '```', true)
+		.addField('Guild Members', '```' + guild.memberCount + '```', true);
+
+	onLeaveChannel.send(onLeaveEmbed);
+}
---
+++ events/mongooseConnect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+module.exports = (mongodb) => {
+    mongoose.set('useCreateIndex', true);
+
+    mongoose
+	.connect(mongodb, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log('Connected To The MongoDB Database!');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+}
