182d46035a3b70bc83ac76e76977f40ee5553433
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-06-13 19:36:02 +0500

    Add a check! 🤣

--- commands/staff/changestatus.js
+++ commands/staff/changestatus.js
@@ -5,6 +5,8 @@ module.exports = {
   description: "Change the bot's status.",
   usage: '<status>',
   run: async(client, message, args) => {
+    if(message.author.id !== "763767239018938368") return error("You aren't my owner!", "You don't seem to be my owner, now get outta here!", message);
+
     const status = args.slice(0).join(" ");
 
     if(!status) return error("Please provide a status!", "Give me the status to set.", message);
