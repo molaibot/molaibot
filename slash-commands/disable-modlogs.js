b79076b77b569342b1b2d9fce858d8bca9fda780
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-06-30 03:03:23 +0500

    Add a command to disable modlogs! (its a / command!)

---
+++ slash-commands/disable-modlogs.js
@@ -0,0 +1,35 @@
+const { CommandInteraction, MessageEmbed } = require("discord.js");
+const modlogs = require('../models/modlogs');
+const e = require('../utils/embeds.json');
+module.exports = {
+    name: "disable-modlogs",
+    description: "Disable modlogs",
+    /**
+     * @param {CommandInteraction} command
+     */
+    run: async(client, command) => {
+        command.defer();
+
+        await modlogs.findOne({ Guild: command.guild.id }, async(err, data) => {
+            if(err) return console.log(err);
+
+            const noData = new MessageEmbed()
+            .setColor(e.errColor)
+            .setFooter(e.footer)
+            .setTitle("Modlogs isn't enabled.")
+            .setTimestamp();
+
+            const deleted = new MessageEmbed()
+            .setColor(e.color)
+            .setFooter(e.footer)
+            .setTitle("Successfully disabled modlogs!")
+            .setTimestamp();
+
+            if(!data) return command.editReply({ embeds: [noData] });
+
+            if(data) {
+                data.delete().then(() => command.editReply({ embeds: [deleted] }))
+            }
+        });
+    }
+}
