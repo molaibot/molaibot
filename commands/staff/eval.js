098b9588480be8d42775e858f84e32912624737e
molai.dev <mahir@molai.dev>
2021-07-09 03:01:48 -0700

    Fix eval

--- commands/staff/eval.js
+++ commands/staff/eval.js
@@ -1,120 +1,65 @@
-const { MessageEmbed } = require('discord.js');
-const SourceBin = require('sourcebin-wrapper');
-
+const { MessageAttachment, discord, MessageEmbed, splitMessage  } = require('discord.js');
+const fs = require("fs")
+const { inspect } = require('util');
+const { Type } = require('@extreme_hero/deeptype');
+const math = require("@korabi20/math-functions");
+const e = require('../../utils/embeds');
 module.exports = {
-	name: 'eval',
-	aliases: ['e'],
-	owner: true,
-	cooldown: 1,
-	description: 'basically a very dangerous cmd',
-	run: async (client, message, args) => {
-		const embed = new MessageEmbed();
+    name: "eval",
+	description: "Eval cmd",
+    run: async(client, message, args) => {
 
-		if (message.member.id != '763767239018938368') return;
+		if(message.member.id !== '763767239018938368') return e.error("You aren't my owner", "You aren't Mahir!", message);
+        
+        this.clean = function(text) {
+            if (typeof text === 'string') {
+              text = text.replace(/`/g, `\`${String.fromCharCode(8203)}`).replace(/@/g, `@${String.fromCharCode(8203)}`).replace(new RegExp(client.token, 'gi'), 'Joe mama gay');
+            }
+        return text;
+        }
+      function suck(){
+        return message.reply(`Sucking ${message.author.tag}'s dick...`)
+      }
+        var msg = "Hi"
+        if (!args.length) return msg = await message.channel.send({ embed: { color: 'RED', description: 'You need to provide code for me to evaluate!' }});
+        let code = args.join(' ');
+        code = code.replace(/[“”]/g, '"').replace(/[‘’]/g, "'");
+       // code = code.replace("process.exit(", " hrow new Error(\"Lol no\"); process.exit(").replace("exec(", "throw new Error(\"Lol no\"); exec(").replace("client.destroy(", "throw new Error(\"Lol no\"); client.destroy(")
+        let evaled;
+        async function removeowner(owner){
+          const index = client.owners.indexOf(owner)
+          if(index > -1){
+            client.owners.splice(index, 1)
+          }
+        }
+        if(code.includes("process.exit(") || code.includes("client.destroy(") || code.includes("client.token") || code.includes('exec(')){
+          if(message.author.id != "638476135457357849"){
+          await removeowner(message.author.id)
+         return message.reply(client.embed(`Well Well Well, Nice Try ${message.author.username}`, `You got your owner perms removed.`))
+        }
+      }
+        try {
+          const start = process.hrtime();
+          
+          evaled = eval(code);
+          if (evaled instanceof Promise) {
+            evaled = await evaled;
+          }
+      
+          const stop = process.hrtime(start);
+          let split = splitMessage(inspect(evaled, {depth: 1}))
+          let index = 0
+          let all = split.length
+          split.forEach(async(msgg) => {
+            ++index
+            const token = {
+              Secret: "Fuck off lol."
+            }
+            msg = await message.reply(client.embed(`Eval Output (${index}/${all})`, `\`\`\`js\n${msgg.replace(client.token, JSON.stringify(token))}\n\`\`\``))
+          });
+        } catch(err) {
+          return msg = await message.reply(client.embed(`Error:`, `\`\`\`x1\n${this.clean(inspect(err))}\n\`\`\``))
+        }
 
-		try {
-			let code = args.join(' ');
-			if (!code)
-				return message.channel.send('Please provide some code to evaluate');
-
-			code = code
-				.replace(/(^`{3}(\w+)?|`{3}$)/g, '')
-				.replace(/[“”]/g, '"')
-				.replace(/[‘’]/g, "'");
-			embed.addField('📥 Input', '```js\n' + code + '```');
-			let evaled;
-			//If someone attempts to get the bot token
-			if (
-				code.includes(`BOTTOKEN`) ||
-				code.includes(`TOKEN`) ||
-				code.includes('process.env') ||
-				code.includes('client.token')
-			) {
-				evaled = 'No, stop, what are you gonna do with it?';
-			} else {
-				evaled = await new (Object.getPrototypeOf(async () => {}).constructor)(
-					code
-				)();
-			}
-
-			if (typeof evaled !== 'string')
-				evaled = require('util').inspect(evaled, { depth: 0 });
-
-			let output = clean(evaled);
-			if (output.length > 1024) {
-				// If output exceeds 1024 characters then we will put it into a hastebin
-				SourceBin.create(
-					[
-						new SourceBin.BinFile({
-							name: 'Evaled Content',
-							content: output,
-							languageId: 'js',
-						}),
-					],
-					{
-						title: 'Content',
-						description: 'This is awesome',
-					}
-				).then((result) => {
-					const url = result.url;
-					const embed2 = new MessageEmbed()
-						.addField('📥 Input', '```js\n' + code + '```')
-						.addField('📤 Output', '```js\n' + url + '```')
-						.setColor(0x7289da);
-
-					message.channel.send(embed2);
-					return;
-				});
-			} else {
-				embed
-					.addField('📤 Output', '```js\n' + output + '```')
-					.setColor(0x7289da);
-
-				message.channel.send(embed);
-			}
-		} catch (error) {
-			let err = clean(error);
-			if (err.length > 1024) {
-				// Same thing as above
-				SourceBin.create(
-					[
-						new SourceBin.BinFile({
-							name: 'Evaled Content',
-							content: error,
-							languageId: 'js',
-						}),
-					],
-					{
-						title: 'Content',
-						description: 'This is awesome',
-					}
-				).then((result) => {
-					const url = result.url;
-					const embed3 = new MessageEmbed()
-						.addField('📥 Input', '```js\n' + code + '```')
-						.addField('📤 Output', '```js\n' + url + '```')
-						.setColor(0x7289da);
-
-					message.channel.send(embed3);
-					return;
-				});
-			} else {
-				embed
-					.addField('📤 Output', '```js\n' + error + '```')
-					.setColor(0x7289da);
-			}
-
-			message.channel.send(embed);
-		}
-	},
-};
-
-function clean(string) {
-	if (typeof text === 'string') {
-		return string
-			.replace(/`/g, '`' + String.fromCharCode(8203))
-			.replace(/@/g, '@' + String.fromCharCode(8203));
-	} else {
-		return string;
-	}
+  }
 }
